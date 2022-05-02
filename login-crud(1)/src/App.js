f5c61f8bdcfdad8730ede8b151060bcbe6cfbcc3
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-05-02 07:03:15 -0300

    Add files via upload

--- login-crud(1)/src/App.js
+++ login-crud(1)/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Axios from 'axios'
-
+import { useState } from 'react'
 function App() {
-  
-  function getData(){
-    Axios.get('http://localhost:8080')
-    .then(Response => Response.data)
-    .then(data => console.log(data))
+  const [user, setUser] = useState('')
+  const [emailAccount, setEmail] = useState('')
+  const [pswd, setPswd] = useState('')
+
+
+  function insertData(){
+    Axios.post('http://localhost:8080/register', {
+      nome:user, //nome is the field name in DB as senha and email are.
+      senha:pswd, 
+      email:emailAccount,
+    });
   }
 
   return (
     <div className="App">
-      <button onClick={getData}>Pegar dados</button>
+      <button onClick={insertData}>Insert data</button>
+      <input  type='text' placeholder='Username' onChange={e => setUser(e.target.value)}></input>
+      <input  type='email' placeholder='Email' onChange={e => setEmail(e.target.value)}></input>
+      <input  type='password' placeholder='Password' onChange={e => setPswd(e.target.value)}></input>
     </div>
   );
 }
